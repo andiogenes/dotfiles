428eba9462f5c4ef33ff5fc5e0460884b5a4fc56
Anton Zavyalov <megadeathlightsaber@gmail.com>
2021-03-18 19:23:17 +0700

    update

--- refresh.sh
+++ refresh.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-cp -r ~/.config/polybar ~/.config/i3 ~/.config/i3status .
+POLYBAR_DIR=$HOME/.config/polybar
+I3_DIR=$HOME/.config/i3
+ALACRITTY_DIR=$HOME/.config/alacritty
+
+cp -r $POLYBAR_DIR $I3_DIR $ALACRITTY_DIR .

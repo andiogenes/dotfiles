a6c70f216ed5d686435316637751b957943d8ac3
Anton Zavyalov <megadeathlightsaber@gmail.com>
2021-06-29 11:24:14 +0700

    window shadows and transparency via picom

--- refresh.sh
+++ refresh.sh
@@ -5,8 +5,9 @@ I3_DIR=$HOME/.config/i3
 ALACRITTY_DIR=$HOME/.config/alacritty
 EMACS_FILE=$HOME/.emacs
 EMACS_DIR=$HOME/.emacs.d
+PICOM_DIR=$HOME/.config/picom
 
-cp -r $POLYBAR_DIR $I3_DIR $ALACRITTY_DIR .
+cp -r $POLYBAR_DIR $I3_DIR $ALACRITTY_DIR $PICOM_DIR .
 
 cp $EMACS_FILE .
 cp $EMACS_DIR/poggers-theme.el ./.emacs.d/

032fcc91e43fe979376f4b250399ed48f97aa779
Anton Zavyalov <megadeathlightsaber@gmail.com>
2021-09-21 02:06:11 +0700

    remove emacs configuration

--- refresh.sh
+++ refresh.sh
@@ -3,11 +3,6 @@
 POLYBAR_DIR=$HOME/.config/polybar
 I3_DIR=$HOME/.config/i3
 ALACRITTY_DIR=$HOME/.config/alacritty
-EMACS_FILE=$HOME/.emacs
-EMACS_DIR=$HOME/.emacs.d
 PICOM_DIR=$HOME/.config/picom
 
 cp -r $POLYBAR_DIR $I3_DIR $ALACRITTY_DIR $PICOM_DIR .
-
-cp $EMACS_FILE .
-cp $EMACS_DIR/poggers-theme.el ./.emacs.d/
